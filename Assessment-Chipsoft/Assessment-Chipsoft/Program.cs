e643b477df324bd384c01648196822af0e3d31d1
SirRolf <flomo123456789@gmail.com>
2026-08-13 13:45:34 +0200

    Added a backlog system. Added more comments to explain. Post now correctly adds its patient info in essence replacing it

--- Assessment-Chipsoft/Assessment-Chipsoft/Program.cs
+++ Assessment-Chipsoft/Assessment-Chipsoft/Program.cs
@@ -4,21 +4,32 @@ using Microsoft.AspNetCore.Http.HttpResults;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-Dictionary<int, PatientInfo?> patients = [];//For this task in memory data is acceptable so will implement a database only if I have time left over
+Dictionary<int, List<PatientInfo>> patients = [];//For this task in memory data is acceptable so will implement a database only if I have time left over
 
 app.MapGet("/PatientDatabase/{id}", Results<Ok<PatientInfo>, NotFound> (int id) =>//Authorization not necessary as it falls outside the scope for this task
 {
-	if (!patients.TryGetValue(id, out PatientInfo? patient))
+	//Try and find the patient by id  to see if we have him in memory
+	if (!patients.TryGetValue(id, out List<PatientInfo>? patient))
 	{
+		//If not then return 404
 		return TypedResults.NotFound();
 	}
-	return TypedResults.Ok(patient);
+	//if patient is found then return patient with 200
+	return TypedResults.Ok(patient.Last());//Returning only the last Patient Info. Could consider giving the entire list.
 });
 
 app.MapPost("/PatientDatabase", (PatientInfo info) =>
 {
-	//TODO: Add check if patient is already in the Database
-	patients.Add(info.Id,info);
+	//instead of having just one PatientInfo i create a backlog for every patient. In case some human error gets made we would still have precious data of patient.
+	if (!patients.TryGetValue(info.Id, out List<PatientInfo>? value))
+	{
+		List<PatientInfo> patientList = [info];
+		patients.Add(info.Id, patientList);
+	}
+	else
+	{
+        value.Add(info);
+	}
 	return TypedResults.Created("/PatientDatabase/{id}", patients);
 });
 
