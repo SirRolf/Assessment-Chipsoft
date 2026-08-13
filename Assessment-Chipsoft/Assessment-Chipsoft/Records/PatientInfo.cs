75e61c7b832964f44ff1589b0c115a18e025c14d
SirRolf <flomo123456789@gmail.com>
2026-08-13 13:56:51 +0200

    added summary and explenations to PatientInfo record

--- Assessment-Chipsoft/Assessment-Chipsoft/Records/PatientInfo.cs
+++ Assessment-Chipsoft/Assessment-Chipsoft/Records/PatientInfo.cs
@@ -1,3 +1,9 @@
 ﻿namespace Assessment_Chipsoft.Records;
 
-public record PatientInfo(int Id, string Name, List<string> Allergies);
+/// <summary>
+/// record containing all information for given patient
+/// </summary>
+/// <param name="Id">ID of patient</param>
+/// <param name="Name">Name of patient</param>
+/// <param name="Allergies">string of allergies by name</param>
+public record PatientInfo(int Id, string Name, List<string> Allergies);//chose to use a list of strings as opposed to enums because of the wide range of niche allergies
