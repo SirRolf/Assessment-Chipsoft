e9b155f3c3da9617e945a17a1aeec0a02bbcd93c
SirRolf <flomo123456789@gmail.com>
2026-08-19 15:46:10 +0200

    Moved endpoints to their own classes

---
+++ Assessment-Chipsoft/Assessment-Chipsoft/Endpoints/GetPatientInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.Json;
+using Assessment_Chipsoft.Records;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Assessment_Chipsoft.Endpoints;
+
+public static class GetPatientInfo
+{
+	public static void MapGetLatestPatientInfo(this IEndpointRouteBuilder app, Dictionary<int, List<PatientInfo>> patients, StringBuilder sb)
+	{
+		//receive latest patient info with given id
+		app.MapGet("/PatientDatabase/{id}", Results<Ok<PatientInfo>, NotFound> (int id) =>//Authorization not necessary as it falls outside the scope for this task
+		{
+			//Try and find the patient by id to see if we have him in memory
+			if (!patients.TryGetValue(id, out List<PatientInfo>? patient))
+			{
+				sb.AppendLine($"Patient with id: {id} not found");
+		
+				//If not then return 404
+				return TypedResults.NotFound();
+			}
+	
+			//Return latest patient info
+			PatientInfo info = patient.Last();
+			//append result and 
+			sb.AppendLine($"Patient with id: {id} found and returned");
+			sb.AppendLine(JsonSerializer.Serialize(info));
+	
+			//if patient is found then return patient with 200
+			return TypedResults.Ok(info);
+		});
+	}
+}
---
+++ Assessment-Chipsoft/Assessment-Chipsoft/Endpoints/UploadNewPatient.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.Json;
+using Assessment_Chipsoft.Records;
+
+namespace Assessment_Chipsoft.Endpoints;
+
+public static class UploadNewPatient
+{
+	public static void MapUploadNewPatient(this IEndpointRouteBuilder app, Dictionary<int, List<PatientInfo>> patients, StringBuilder sb)
+	{
+		//uploading new patient
+		app.MapPost("/PatientDatabase", (PatientInfo info) =>
+		{
+			//Try and find the patient by id to see if we have him in memory
+			if (!patients.TryGetValue(info.Id, out List<PatientInfo>? value))
+			{
+				//if the patient doesn't exist
+				sb.AppendLine($"Creating new patient in data with id: {info.Id}");
+				//create a new list for instances of PatientInfo and add it to the dictionary
+				List<PatientInfo> patientList = [info];
+				patients.Add(info.Id, patientList);
+			}
+			else
+			{
+				//if the patient does exist
+				sb.AppendLine($"Patient with id: {info.Id} already exists");
+				//add the new value to the patient
+				value.Add(info);
+			}
+	
+			sb.AppendLine(JsonSerializer.Serialize(info));
+	
+			//return 200
+			return TypedResults.Created("/PatientDatabase/{id}", info);
+		});
+	}
+}
